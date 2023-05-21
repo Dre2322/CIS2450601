e4f76e33eae43428a0ff66c483491fc9b0020fde
Dre2322 <133067735+Dre2322@users.noreply.github.com>
2023-05-21 00:52:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+def main ():
+  intro()
+  try :
+    mile_need = float(input('Enter the number of miles: '))
+
+  except :
+    print('An error occured, try again using only numbers.')
+    print()
+    main()
+
+  else :
+     mile_to_km(mile_need)
+
+
+def intro():
+  print('This program converts miles into kilometers')
+  print()
+
+def mile_to_km(mile):
+  km = mile * 1.61
+  print (mile, 'miles converts to ', km, 'kilometers.')
+
+main()
